57d573c7bdddb82b966ac07bd4f4a589acc55b53
Marcio Silva <marcio.silva@gmail.com>
2013-02-22 01:11:15 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-hadoopjar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen Plugin for generating Hadoop job jars"
+  :url "http://github.com/marciosilva/lein-hadoopjar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
